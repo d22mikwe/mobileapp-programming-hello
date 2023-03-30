fbb27afab4de49864b8c2942cd17508cb22b7d17
d22mikwe <d22mikwe@student.his.se>
2023-03-30 12:00:17 +0200

    Added a button. Button changes color to red when pressed

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.webviewapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v){
+            int red = Color.parseColor("#FF0000");
+
+            button.setBackgroundColor(red);
+        }
+
+        });
     }
 
 }
